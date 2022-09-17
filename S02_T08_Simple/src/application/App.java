7111be8af75be5471fbe5a0f8130b3c7714391f0
CharlesKirkSD <charlesackirk@gmail.com>
2022-09-17 13:30:36 +0100

    Simple : v1.0 : Added 'At 13:30'

---
+++ S02_T08_Simple/src/application/App.java
@@ -0,0 +1,12 @@
+package application;
+
+public class App {
+
+    public static void main(String [] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("On Saturday 17th September 2022");
+        System.out.println("At 13:30");
+
+    }
+}
