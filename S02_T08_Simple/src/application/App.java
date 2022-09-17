83adb1725e263b326f88976b34d7880c1daeeb6f
CharlesKirkSD <charlesackirk@gmail.com>
2022-09-17 13:26:22 +0100

    Simple : v1.0 : Added 'On Saturday 17th September 2022'

--- S02_T08_Simple/src/application/App.java
+++ S02_T08_Simple/src/application/App.java
@@ -5,6 +5,7 @@ public class App {
     public static void main(String [] args) {
 
         System.out.println("Hello World!");
+        System.out.println("On Saturday 17th September 2022");
 
     }
 }
