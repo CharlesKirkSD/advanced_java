b6ed711667f73982c9f564bfce521ee82c6fa276
CharlesKirkSD <charlesackirk@gmail.com>
2022-09-26 10:47:31 +0100

    Added new projects from Section 3 of the course

---
+++ S02_T19_EclipseAndGit/src/application/App.java
@@ -0,0 +1,11 @@
+package application;
+
+public class App {
+	
+	public static void main(String [] args) {
+		
+		
+		
+		System.out.println("Hello World!!! from Eclipse for Java Developers");
+	}
+}
