7c00f8c302c9bcdd832b893b1659c68d9010801d
CharlesKirkSD <charlesackirk@gmail.com>
2022-10-13 20:09:51 +0100

    S06_T57-T60 : Added 4 projects
    T57 Maven Project for My SQL, T58 Downloading MySQL Connector, T59 The Singleton Pattern, T60 Implementing the Database Class

---
+++ S06_T58_Download_MySQL_Connector/src/main/java/com/ck/App.java
@@ -0,0 +1,7 @@
+package com.ck;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!\nfrom S06_T58");
+	}
+}
---
+++ S06_T59_The_Singleton_Pattern/src/main/java/com/ck/App.java
@@ -0,0 +1,9 @@
+package com.ck;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!\nfrom S06_T59");
+		
+		Database db = Database.instance();
+	}
+}
