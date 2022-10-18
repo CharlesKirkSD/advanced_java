e6a6a1638a7a2ab6b8af12a28d2b9618d64cb18e
CharlesKirkSD <charlesackirk@gmail.com>
2022-10-18 10:55:00 +0100

    S06_T66_Retrieving_Users : Complete Tutorial Implementation

---
+++ S06_T66_Retrieving_Users/src/main/java/com/ck/App.java
@@ -0,0 +1,40 @@
+package com.ck;
+
+import java.sql.SQLException;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!\nfrom S06_T66");
+		
+		Database db = Database.instance();
+		try {
+			db.connect();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Cannot connect to the database.");
+			e.printStackTrace();
+		}
+		
+		System.out.println("Connected");
+
+		// Add users to the database
+		UserDao userDao = new UserDaoImpl();
+		/*
+		userDao.save(new User("Uranus"));
+		userDao.save(new User("Mercury"));
+		*/
+		
+		var users = userDao.getAll();
+		
+		users.forEach(System.out::println);
+		
+		try {
+			db.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Cannot close database connection.");
+			e.printStackTrace();
+		}
+		
+	}
+}
