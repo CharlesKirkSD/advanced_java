ecd0d24d943fe538d8e8edd2749254d61dd661cb
CharlesKirkSD <charlesackirk@gmail.com>
2022-10-25 11:02:36 +0100

    S08_T79_Loading_a_Text_File

--- S08_T79_Loading_a_Text_File/src/test/java/com/ck/UserDaoImplTest.java
+++ S08_T79_Loading_a_Text_File/src/test/java/com/ck/UserDaoImplTest.java
@@ -3,8 +3,12 @@ package com.ck;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
@@ -12,10 +16,20 @@ import org.junit.Test;
 
 public class UserDaoImplTest {
 	private Connection conn;
+	private List<User> users;
+	private static final int NUM_TEST_USERS = 1000;
+	
+	private List<User> loadUsers() throws IOException {
+		var temp = Files.lines(Paths.get("../greatexpectations.txt"));
+		temp.forEach(System.out::println);
+		return null;
+	}
 	
 	@Before
-	public void setUp() throws SQLException {
+	public void setUp() throws SQLException, IOException {
 		System.out.println("Before"); // Confirm these methods are called.
+		
+		users = loadUsers();
 		var props = Profile.getProperties("db"); // Comes from the properties of the environment, the System properties, these need to be set before the test is run.
 		var db = Database.instance();
 		db.connect(props);
@@ -32,12 +46,12 @@ public class UserDaoImplTest {
 	@Test
 	public void testSave() throws SQLException {
 		User user = new User("Jupiter");
+		
 		UserDao userDao = new UserDaoImpl();
 		
 		userDao.save(user);
 		
 		var stmt = conn.createStatement();
-		
 		var rs = stmt.executeQuery("select id, name from user order by id");
 		boolean result = rs.next();
 		
