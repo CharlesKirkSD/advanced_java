d5ed805c799e92c50432ca219a0786170e1d5e6b
CharlesKirkSD <charlesackirk@gmail.com>
2022-11-09 17:03:44 +0000

    S11_Swing_Demo : S11_T107 Creating Components : Complete Tutorial Implementation

--- S11_Swing_Demo/app/src/main/java/gui/MainPanel.java
+++ S11_Swing_Demo/app/src/main/java/gui/MainPanel.java
@@ -1,14 +1,32 @@
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class MainPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	
 	public MainPanel() {
-		setBackground(Color.red);
+		setBackground(Color.LIGHT_GRAY);
+		
+		// Create the components
+		JLabel formLabel = new JLabel("Add User");
+		JLabel nameLabel = new JLabel("Name:");
+		JLabel passLabel = new JLabel("Password");
+		
+		JTextField nameField = new JTextField();
+		JTextField passField = new JTextField();
+		
+		JButton addButton = new JButton("Save");
+		
+		setLayout(new BorderLayout());
+		add(formLabel, BorderLayout.NORTH);
+		add(nameLabel, BorderLayout.CENTER);
 	}
 }
