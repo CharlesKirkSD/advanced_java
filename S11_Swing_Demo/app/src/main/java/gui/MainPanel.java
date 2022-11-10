5ff04cba6ef3ebf1ded89e517dfffaf803a1330a
CharlesKirkSD <charlesackirk@gmail.com>
2022-11-10 10:47:10 +0000

    S11_Swing_Demo : S11_T109 Weights and Anchors : Complete Tutorial Implementation

--- S11_Swing_Demo/app/src/main/java/gui/MainPanel.java
+++ S11_Swing_Demo/app/src/main/java/gui/MainPanel.java
@@ -32,22 +32,28 @@ public class MainPanel extends JPanel {
 		
 		gc.gridx = 0;
 		gc.gridy = 0;
+		gc.weightx = 1;
+		gc.weightx = 1;
 		
 		gc.gridwidth = 2;
 		add(formLabel, gc);
 		gc.gridwidth = 1;
 		
 		gc.gridy++;
+		gc.anchor = GridBagConstraints.LINE_END;
 		add(nameLabel, gc);
 		
 		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.LINE_START;
 		add(nameField, gc);
 		
 		gc.gridx = 0;
 		gc.gridy++;
+		gc.anchor = GridBagConstraints.LINE_END;
 		add(passLabel, gc);
 		
 		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.LINE_START;
 		add(passField, gc);
 		
 		gc.gridy++;
