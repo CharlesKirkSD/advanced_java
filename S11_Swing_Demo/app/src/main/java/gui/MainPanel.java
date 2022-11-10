7f55fbd0c59bb5801a7b609282e4c9cd501b8ea5
CharlesKirkSD <charlesackirk@gmail.com>
2022-11-10 10:35:58 +0000

    S11_Swing_Demo : S11_T108 Adding Components : Complete Tutorial Implementation

--- S11_Swing_Demo/app/src/main/java/gui/MainPanel.java
+++ S11_Swing_Demo/app/src/main/java/gui/MainPanel.java
@@ -1,7 +1,8 @@
 package gui;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -16,17 +17,40 @@ public class MainPanel extends JPanel {
 		setBackground(Color.LIGHT_GRAY);
 		
 		// Create the components
-		JLabel formLabel = new JLabel("Add User");
-		JLabel nameLabel = new JLabel("Name:");
-		JLabel passLabel = new JLabel("Password");
+		JLabel formLabel = new JLabel("Add User", JLabel.RIGHT);
+		JLabel nameLabel = new JLabel("Name:", JLabel.RIGHT);
+		JLabel passLabel = new JLabel("Password:", JLabel.RIGHT);
 		
-		JTextField nameField = new JTextField();
-		JTextField passField = new JTextField();
+		JTextField nameField = new JTextField(15);
+		JTextField passField = new JTextField(15);
 		
 		JButton addButton = new JButton("Save");
 		
-		setLayout(new BorderLayout());
-		add(formLabel, BorderLayout.NORTH);
-		add(nameLabel, BorderLayout.CENTER);
+		setLayout(new GridBagLayout());
+		
+		GridBagConstraints gc = new GridBagConstraints();
+		
+		gc.gridx = 0;
+		gc.gridy = 0;
+		
+		gc.gridwidth = 2;
+		add(formLabel, gc);
+		gc.gridwidth = 1;
+		
+		gc.gridy++;
+		add(nameLabel, gc);
+		
+		gc.gridx = 1;
+		add(nameField, gc);
+		
+		gc.gridx = 0;
+		gc.gridy++;
+		add(passLabel, gc);
+		
+		gc.gridx = 1;
+		add(passField, gc);
+		
+		gc.gridy++;
+		add(addButton, gc);
 	}
 }
