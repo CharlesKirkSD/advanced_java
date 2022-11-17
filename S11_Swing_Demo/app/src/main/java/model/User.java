0e91d70cdd2e6fd01adf62ad9fa2e363d5e06d98
CharlesKirkSD <charlesackirk@gmail.com>
2022-11-17 16:38:37 +0000

    S11_Swing_Demo : S11_T118 Adding Fields : Complete Tutorial Implementation : The JUnit tests are not working. They Will be updated in the next tutorial.

--- S11_Swing_Demo/app/src/main/java/model/User.java
+++ S11_Swing_Demo/app/src/main/java/model/User.java
@@ -3,35 +3,10 @@ package model;
 import java.util.Objects;
 
 public class User {
-	@Override
-	public int hashCode() {
-		return Objects.hash(id, name);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		return id == other.id && Objects.equals(name, other.name);
-	}
-
 	private int id;
 	private String name;
+	private String password;
 	
-	public User(String name) {
-		this.name = name;
-	}
-	
-	public User(int id, String name) {
-		this.id = id;
-		this.name = name;
-	}
-
 	public int getId() {
 		return id;
 	}
@@ -48,9 +23,44 @@ public class User {
 		this.name = name;
 	}
 
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + "]";
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public User(String name, String password) {
+		this.name = name;
+		this.password = password;
 	}
 	
+	public User(int id, String name, String password) {
+		this.id = id;
+		this.name = name;
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", password=" + password + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, password);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return id == other.id && Objects.equals(name, other.name) && Objects.equals(password, other.password);
+	}
 }
