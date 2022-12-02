782c30873e5524c2ad183f4a1c7980d51403f91d
CharlesKirkSD <charlesackirk@gmail.com>
2022-12-02 10:32:52 +0000

    S13_T141_Spring_Boot_Hello_World

---
+++ S13_T141_Spring_Boot_Hello_World/src/main/java/app/App.java
@@ -0,0 +1,16 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+	
+	public static void main(String [] args) {
+		
+		System.out.println("Hello World! from S13_T141");
+		
+		SpringApplication.run(App.class, args);
+		
+	}
+}
