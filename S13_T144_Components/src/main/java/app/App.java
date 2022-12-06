9f720a69c06228a1e085c3423cfc25e1aff2a4a3
CharlesKirkSD <charlesackirk@gmail.com>
2022-12-06 09:41:18 +0000

    S13_T144_Components

---
+++ S13_T144_Components/src/main/java/app/App.java
@@ -0,0 +1,14 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"greeters", "main"})
+public class App {
+	
+	public static void main(String [] args) {
+		
+		SpringApplication.run(App.class, args);
+		
+	}
+}
