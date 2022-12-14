1181ca3a4f2af6f8701f4d4b05fd81476cec837f
CharlesKirkSD <charlesackirk@gmail.com>
2022-12-14 10:23:48 +0000

    S14_T156_Posting_JSON_to_Spring

--- S14_T156_Posting_JSON_to_Spring/src/main/java/app/controllers/BookController.java
+++ S14_T156_Posting_JSON_to_Spring/src/main/java/app/controllers/BookController.java
@@ -1,17 +1,26 @@
 package app.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.entities.Book;
 
 @RestController
-public class BookControllers {
+public class BookController {
 
 	@GetMapping("/books")
 	public @ResponseBody Book books() {
 		Book book = new Book("Great Expectations", "Charles Dickens");
+		return book;
+	}
+	
+	@PostMapping("/books")
+	public Book createBook(@RequestBody Book book) {
+		System.out.println("Creating: "  + book);
+		book.setId(7L);
 		return book;
 	}
 }
