463ee748347de8ee58cbb5efe7dc130269a43976
CharlesKirkSD <charlesackirk@gmail.com>
2022-12-14 10:47:28 +0000

    S14_T157_Path_Variables

--- S14_T157_Path_Variables/src/main/java/app/App.java
+++ S14_T157_Path_Variables/src/main/java/app/App.java
@@ -8,7 +8,7 @@ public class App {
 	
 	public static void main(String [] args) {
 		
-		System.out.println("Hellow World! from S14_T156");
+		System.out.println("Hellow World! from S14_T157");
 		
 		SpringApplication.run(App.class, args);
 		
