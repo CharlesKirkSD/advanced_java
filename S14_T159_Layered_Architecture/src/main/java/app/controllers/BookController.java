60f3a83e3d3a21b4c1436879b99904c9ffdebc00
CharlesKirkSD <charlesackirk@gmail.com>
2022-12-15 14:35:22 +0000

    S14_T159_Layered_Architecture

---
+++ S14_T159_Layered_Architecture/src/main/java/app/controllers/BookController.java
@@ -0,0 +1,40 @@
+package app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import app.entities.Book;
+import app.services.BookService;
+
+@RestController
+public class BookController {
+
+	@Autowired
+	private BookService bookService;
+	
+	@GetMapping("/books")
+	public @ResponseBody Book books() {
+		Book book = new Book("Great Expectations", "Charles Dickens");
+		return book;
+	}
+	
+	@PostMapping("/books")
+	public Book createBook(@RequestBody Book book) {
+		System.out.println("Creating: "  + book);
+		book.setId(7L);
+		return book;
+	}
+	
+	@GetMapping("/books/{id}")
+	public Book getBook(@PathVariable("id") Long id) {
+		Book book = new Book("Great Expectations", "Charles Dickens");
+		System.out.println("Book ID: " + id);
+		book.setId(id);
+		return book;
+	}
+}
---
+++ S14_T159_Layered_Architecture/src/main/java/app/entities/Book.java
@@ -0,0 +1,80 @@
+package app.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="books")
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 100, nullable = false)
+	private String title;
+	private String author;
+	
+	// No parameter constructor for JPA Entity required by Hibernate
+	public Book() {
+		
+	}
+
+	public Book(String title, String author) {
+		this.title = title;
+		this.author = author;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(author, id, title);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(author, other.author) && Objects.equals(id, other.id)
+				&& Objects.equals(title, other.title);
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + "]";
+	}
+	
+}
