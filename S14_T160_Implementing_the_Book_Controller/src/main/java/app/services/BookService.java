934e09df29bd22335dca4ec1f8635e9e8cda2f5a
CharlesKirkSD <charlesackirk@gmail.com>
2022-12-16 11:05:38 +0000

    S14_T160_Implementing_the_Book_Controller

---
+++ S14_T160_Implementing_the_Book_Controller/src/main/java/app/services/BookService.java
@@ -0,0 +1,37 @@
+package app.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import app.entities.Book;
+import app.respositories.BookDao;
+
+@Service
+public class BookService {
+	@Autowired
+	private BookDao bookDao;
+	
+	public List<Book> getAll() {
+		List<Book> books = new ArrayList<>();
+		bookDao.findAll().forEach(book -> books.add(book));
+		return books;
+	}
+	
+	public Book getBook(Long id) {
+		var bookOpt = bookDao.findById(id);
+		
+		if (bookOpt.isPresent()) {
+			return bookOpt.get();
+		}
+		
+		System.out.println("No book with id: " + id);
+		return null;		
+	}
+	
+	public void save(Book book) {
+		bookDao.save(book);
+	}
+}
